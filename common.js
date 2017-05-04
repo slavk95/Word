874d234da630212229d10d6eeeeb024695e04339
Viacheslav Olshevskyi <viacheslavolshevskyi@gmail.com>
2017-05-04 18:27:36 +0300

    final version

--- common.js
+++ common.js
@@ -1,48 +1,48 @@
+/*Розв'яжемо дану задачу за допомогою таблиці*/
 function tbl() {
-	var text = document.getElementById("text").value,
-	num = document.getElementById("num").value,
-	tbl = document.getElementById("tbl");
-	var count = text.length - num;
-	var arr = text.split("");
-	
-	for (var i=0; i< num; i++){
-		var elemTr = document.createElement("tr");
-		tbl.appendChild(elemTr);
-		var elemTd = document.createElement("td");
-		elemTr.appendChild(elemTd);
-		var content = document.createTextNode(text.charAt(i));
-		elemTd.appendChild(content);
-		// for (var j=0; j<text.length; j++){
-		// 	var content = document.createTextNode(text.charAt(i));
-		// 	elemTr.appendChild(content);
-		// // alert(text.charAt(i))
-		// }
+	var text = document.getElementById("text").value, //отримуємо введений текст
+	num = document.getElementById("num").value, // отримуємо введене число
+	tbl = document.getElementById("tbl"); // отримуємо нашу таблицю
+	var count = text.length - num; // отримуємо кількість літер, які не вмістилися у перший стовпчик
+	var arr = text.split(""); // розбиваємо нашу стрічку на літери
+	if (num == 1){
+		for (var n=0; n<text.length; n++){
+			var elemTr = document.createElement("tr"); // створюємо рядок таблиці
+			tbl.appendChild(elemTr); // додаємо рядок в таблицю
+			/*розбиваємо рядок таблиці*/
+			var elemTd = document.createElement("td"); 
+			elemTr.appendChild(elemTd);
+			var content = document.createTextNode(text.charAt(n));
+			elemTd.appendChild(content);// додаємо по одній літері у кожен рядок
+		}
 	}
-	var j=0,k=-1;
-	var newArr = arr.slice(num);
-	var lastElemTr = document.getElementsByTagName("tr");
-	for (j=0; j< count; j++){
+	else {
+		for (var i=0; i< num; i++){
+			var elemTr = document.createElement("tr"); // створюємо рядок таблиці
+			tbl.appendChild(elemTr); // додаємо рядок в таблицю
+			/*розбиваємо рядок таблиці*/
+			var elemTd = document.createElement("td"); 
+			elemTr.appendChild(elemTd);
+			var content = document.createTextNode(text.charAt(i));
+			elemTd.appendChild(content);// додаємо по одній літері у кожен рядок
+		}
+		var j=0,k=-1; // k - це змінна, яку ми використали для того, щоб коли будемо додавати літери, і в нас під час
+		//цього закінчаться рядки для розбиття тегом td, то розбиття почнеться знову спочатку.
+		var newArr = arr.slice(num);// обрізаємо рядок так, щоб залишилися літери, які ще не додалися
+		var lastElemTr = document.getElementsByTagName("tr"); // отримуємо всі рядки
+		for (j=0; j< count; j++){
 			k++;
 			if (lastElemTr[k] == undefined) {
 				k=0;
 			}
 			var lastElemTd = document.createElement("td");
 			lastElemTr[k].appendChild(lastElemTd);
-			var lastContent = document.createTextNode((newArr[j])); //перерив - е,р,и,в
+			var lastContent = document.createTextNode((newArr[j])); 
 			lastElemTd.appendChild(lastContent);
-
-
-			//lastElemTr[j].appendChild(elemTd.appendChild(lastContent));
-			
-			// elemNew[j].appendChild(elemTd);
-			// alert(text.charAt(3));
-			// alert(newArr[j]);
-			
 		}
-
-
+	}
 }
 var inp = document.getElementsByTagName("input");
-inp[1].addEventListener("change", tbl, false)
+inp[1].addEventListener("change", tbl, false); // стандартна модель обробника подій
 
 
